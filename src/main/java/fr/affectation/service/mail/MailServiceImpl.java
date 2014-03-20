f9f7961edb295b7af6feb61a3352470e670801dd
Jean Feutrie <jean.feutrie@centrale-marseille.fr>
2014-03-20 13:15:08 +0100

    Need to repare bug called filterChainProxy

--- src/main/java/fr/affectation/service/mail/MailServiceImpl.java
+++ src/main/java/fr/affectation/service/mail/MailServiceImpl.java
@@ -7,10 +7,6 @@ import javax.inject.Inject;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-<<<<<<< HEAD
-
-=======
->>>>>>> 4a5f279c9a9869c58bea28c4bdc6a78147484cbf
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
@@ -21,54 +17,54 @@ import fr.affectation.domain.util.SimpleMail;
 
 @Service
 public class MailServiceImpl implements MailService {
-	
-	@Resource(name="from")
-	private String from;
-	
-	@Inject
-	private JavaMailSenderImpl mailSender;
-	
-	@Inject
-	private SessionFactory sessionFactory;
-	
-	@Override
-	public void sendMail(String from, String to, String subject, String body) {
-		SimpleMailMessage message = new SimpleMailMessage();
-		message.setFrom(from);
-		message.setTo(to);
-		message.setSubject(subject);
-		message.setText(body);
-		mailSender.send(message);       
-	}
 
-	@Override
-	@Transactional(readOnly=true)
-	public Mail getFirstMail() {
-		Session session = sessionFactory.getCurrentSession();
-		Mail firstMail = (Mail) session.get(Mail.class, (long) 1);
-		return firstMail != null ? firstMail : new Mail((long) 1, "Object", "Message");
-	}
+    @Resource(name="from")
+    private String from;
 
-	@Override
-	@Transactional(readOnly=true)
-	public Mail getSecondMail() {
-		Session session = sessionFactory.getCurrentSession();
-		Mail secondMail = (Mail) session.get(Mail.class, (long) 2);
-		return secondMail != null ? secondMail : new Mail((long) 2, "Object", "Message");
-	}
+    @Inject
+    private JavaMailSenderImpl mailSender;
 
-	@Override
-	@Transactional
-	public void save(Mail mail) {
-		Session session = sessionFactory.getCurrentSession();
-		session.saveOrUpdate(mail);
-	}
+    @Inject
+    private SessionFactory sessionFactory;
 
-	@Override
-	public void sendSimpleMail(SimpleMail mail, List<String> addressees) {
-		for (String login : addressees){
-			sendMail(from, login + "@centrale-marseille.fr", mail.getObject(), mail.getMessage());
-		}
-	}
+    @Override
+    public void sendMail(String from, String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+    }
+
+    @Override
+    @Transactional(readOnly=true)
+    public Mail getFirstMail() {
+        Session session = sessionFactory.getCurrentSession();
+        Mail firstMail = (Mail) session.get(Mail.class, (long) 1);
+        return firstMail != null ? firstMail : new Mail((long) 1, "Object", "Message");
+    }
+
+    @Override
+    @Transactional(readOnly=true)
+    public Mail getSecondMail() {
+        Session session = sessionFactory.getCurrentSession();
+        Mail secondMail = (Mail) session.get(Mail.class, (long) 2);
+        return secondMail != null ? secondMail : new Mail((long) 2, "Object", "Message");
+    }
+
+    @Override
+    @Transactional
+    public void save(Mail mail) {
+        Session session = sessionFactory.getCurrentSession();
+        session.saveOrUpdate(mail);
+    }
+
+    @Override
+    public void sendSimpleMail(SimpleMail mail, List<String> addressees) {
+        for (String login : addressees){
+            sendMail(from, login + "@centrale-marseille.fr", mail.getObject(), mail.getMessage());
+        }
+    }
 
 }
